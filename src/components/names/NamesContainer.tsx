46b1ff16647272abf6e455a2eabbb1981ef96ab6
Teemu K <teemu.kaunisto@gmail.com>
2021-01-23 15:52:24 +0200

    Link NamesViewSelector to NamesContainer
    use NamesViewSelector to change between NamesCards and NamesTable

--- src/components/names/NamesContainer.tsx
+++ src/components/names/NamesContainer.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { createUseStyles, useTheme } from "react-jss"
-import { useHistory, useParams, useLocation } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import NameCard from "./NameCard"
+import NamesTable from "./NamesTable"
+import NamesViewSelector from "./NamesViewSelector"
 
 type Props = {
     names :Name[]
@@ -14,28 +16,24 @@ const useStyles = createUseStyles((theme :Theme) => ({
         flexFlow: 'wrap',
         justifyContent : 'space-between'
     },
-
 }))
 
 function NamesContainer({names} :Props) {
 
     const [filteredNames, setFilteredNames] = useState<Name[]>(names)
+    const [cardView, setCardView] = useState<boolean>(true)
 
     const { id } = useParams<{id : string}>()
+
     const history = useHistory()
+    const theme = useTheme()
+    const classes = useStyles({theme})
 
     useEffect(() => {
 
-        console.log('useEffect names container')
-        console.log(id)
-        console.log(names)
-
         if (id) {
-
             const foundNode = names.find(name => name.id === parseInt(id))
             foundNode && setFilteredNames([foundNode])
-
-            console.log(foundNode)
 
         } else {
             setFilteredNames(names)
@@ -47,19 +45,21 @@ function NamesContainer({names} :Props) {
         history.push(`/names/${id}`)
     }
 
-    const theme = useTheme()
-    const classes = useStyles({theme})
+    const changeView = (_cardView :boolean) => {
+        if (cardView !== _cardView) {
+            setCardView(_cardView)
+        }
+    }
     
     return (
         <>
             <div className={classes.container}>
-                {filteredNames.map((name) => <NameCard key={name.id} {...{...name, route}}></NameCard>)}
+                <NamesViewSelector {...{changeView}} />
+                {cardView && filteredNames.map((name) => <NameCard key={name.id} {...{...name, route}}></NameCard>)}
+                {!cardView && <NamesTable {...{names : filteredNames, route}}></NamesTable>}
             </div>
         </>
     )
-    
-
-
 
 }
 
